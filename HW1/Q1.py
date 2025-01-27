348f402b6b1bb758a4cff9ba3162fa7e27836710
fpvellacgt <156336253+fpvellacgt@users.noreply.github.com>
2025-01-27 10:57:06 -0500

    Add files via upload

---
+++ HW1/Q1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 10:53:42 2025
+
+@author: fcvella
+"""
+
---
+++ HW1/q1/Q1.out.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 10:54:12 2025
+
+@author: fcvella
+"""
+
